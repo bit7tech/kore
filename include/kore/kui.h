7f94333e4f25bec19df1cd37a2b99db3638d1148
Matthew Davies <mattie.davies@gmail.com>
2019-06-13 08:35:27 -0400

    Added keys and key routines.

--- include/kore/kui.h
+++ include/kore/kui.h
@@ -73,6 +73,111 @@ void windowAddEvent(Window* window, const WindowEvent* event);  // Add an event 
 void windowAddGlobalEvent(const WindowEvent* event);            // Add an event not related to a window or referred
 
 //----------------------------------------------------------------------------------------------------------------------
+// Keyboard API
+//----------------------------------------------------------------------------------------------------------------------
+
+// Some convenience routines for checking keys in events
+bool keyPressed(const WindowEvent* ev, int key);          // YES if a particular key was pressed with no shifts.
+bool keyShiftPressed(const WindowEvent* ev, int key);     // YES if a particular key was pressed with just SHIFT.
+bool keyCtrlPressed(const WindowEvent* ev, int key);      // YES if a particular key was pressed with just CTRL.
+bool keyAltPressed(const WindowEvent* ev, int key);       // YES if a particular key was pressed with just ALT.
+
+#define     KEY_BACKSPACE   0x08
+#define     KEY_TAB         0x09
+#define     KEY_ENTER       0x0d
+#define     KEY_PAUSE       0x13
+#define     KEY_ESCAPE      0x1b
+#define     KEY_SPACE       0x20
+#define     KEY_PAGEUP      0x21
+#define     KEY_PAGEDOWN    0x22
+#define     KEY_END         0x23
+#define     KEY_HOME        0x24
+#define     KEY_LEFT        0x25
+#define     KEY_UP          0x26
+#define     KEY_RIGHT       0x27
+#define     KEY_DOWN        0x28
+#define     KEY_PRTSC       0x2c
+#define     KEY_INSERT      0x2d
+#define     KEY_DELETE      0x2e
+#define     KEY_0           0x30
+#define     KEY_1           0x31
+#define     KEY_2           0x32
+#define     KEY_3           0x33
+#define     KEY_4           0x34
+#define     KEY_5           0x35
+#define     KEY_6           0x36
+#define     KEY_7           0x37
+#define     KEY_8           0x38
+#define     KEY_9           0x39
+#define     KEY_A           0x41
+#define     KEY_B           0x42
+#define     KEY_C           0x43
+#define     KEY_D           0x44
+#define     KEY_E           0x45
+#define     KEY_F           0x46
+#define     KEY_G           0x47
+#define     KEY_H           0x48
+#define     KEY_I           0x49
+#define     KEY_J           0x4a
+#define     KEY_K           0x4b
+#define     KEY_L           0x4c
+#define     KEY_M           0x4d
+#define     KEY_N           0x4e
+#define     KEY_O           0x4f
+#define     KEY_P           0x50
+#define     KEY_Q           0x51
+#define     KEY_R           0x52
+#define     KEY_S           0x53
+#define     KEY_T           0x54
+#define     KEY_U           0x55
+#define     KEY_V           0x56
+#define     KEY_W           0x57
+#define     KEY_X           0x58
+#define     KEY_Y           0x59
+#define     KEY_Z           0x5a
+#define     KEY_NUM0        0x60
+#define     KEY_NUM1        0x61
+#define     KEY_NUM2        0x62
+#define     KEY_NUM3        0x63
+#define     KEY_NUM4        0x64
+#define     KEY_NUM5        0x65
+#define     KEY_NUM6        0x66
+#define     KEY_NUM7        0x67
+#define     KEY_NUM8        0x68
+#define     KEY_NUM9        0x69
+#define     KEY_MULTIPLY    0x6a
+#define     KEY_ADD         0x6b
+#define     KEY_SUBTRACT    0x6d
+#define     KEY_POINT       0x6e
+#define     KEY_DIVIDE      0x6f
+#define     KEY_F1          0x70
+#define     KEY_F2          0x71
+#define     KEY_F3          0x72
+#define     KEY_F4          0x73
+#define     KEY_F5          0x74
+#define     KEY_F6          0x75
+#define     KEY_F7          0x76
+#define     KEY_F8          0x77
+#define     KEY_F9          0x78
+#define     KEY_F10         0x79
+#define     KEY_F11         0x7a
+#define     KEY_F12         0x7b
+#define     KEY_F13         0x7c
+#define     KEY_F14         0x7d
+#define     KEY_F15         0x7e
+#define     KEY_F16         0x7f
+#define     KEY_F17         0x80
+#define     KEY_F18         0x81
+#define     KEY_F19         0x82
+#define     KEY_F20         0x83
+#define     KEY_F21         0x84
+#define     KEY_F22         0x85
+#define     KEY_F23         0x86
+#define     KEY_F24         0x87
+#define     KEY_NUMLOCK     0x90
+#define     KEY_SCROLLLOCK  0x91
+
+//----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
@@ -690,6 +795,34 @@ void windowAddEvent(Window* window, const WindowEvent* event)
 void windowAddGlobalEvent(const WindowEvent* event)
 {
     *arrayExpand(g_globalEvents, 1) = *event;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool keyPressed(const WindowEvent* ev, int key)
+{
+    return ev.input.down && !ev.input.shift && !ev.input.ctrl && !ev.input.alt && ev.input.key == key;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool keyShiftPressed(const WindowEvent* ev, int key)
+{
+    return ev.input.down && ev.input.shift && !ev.input.ctrl && !ev.input.alt && ev.input.key == key;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool keyCtrlPressed(const WindowEvent* ev, int key)
+{
+    return ev.input.down && !ev.input.shift && ev.input.ctrl && !ev.input.alt && ev.input.key == key;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool keyAltPressed(const WindowEvent* ev, int key)
+{
+    return ev.input.down && !ev.input.shift && !ev.input.ctrl && ev.input.alt && ev.input.key == key;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
